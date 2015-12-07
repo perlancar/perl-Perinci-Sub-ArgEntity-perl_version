4b2a3e61619e41ebf8fce5b5d36d64b350633f55
perlancar (@pc-home) <perlancar@gmail.com>
2015-12-07 09:22:17 +0700

    pc

---
+++ lib/Perinci/Sub/ArgEntity/perl_version.pm
@@ -0,0 +1,23 @@
+package Perinci::Sub::ArgEntity::perl_version;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Complete::Perl qw(complete_perl_version);
+
+sub complete_arg_val {
+    Complete::Riap::complete_perl_version(@_);
+}
+
+1;
+#ABSTRACT: Data and code related to function arguments of entity type 'perl_version'
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SEE ALSO
+
+L<Perinci::Sub::ArgEntity>
